df98ddd3bf2bf242acdb1a3d4ab1e4bbc3e0c3fd
lein33 <sleandrojoel@hotmail.com>
2025-04-04 18:30:31 -0500

    ss

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo 'Running collecstatic...'
 pwd
 ls
 cat .env
+php artisan bagisto:install --skip-env-check --skip-admin-creation
 php artisan migrate --force
 php artisan db:seed
 #php artisan serve --host=$HOST --port=80
