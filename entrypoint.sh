0fef1c3884b771175532ef467d3d16c41259b806
lein33 <sleandrojoel@hotmail.com>
2025-04-04 19:11:02 -0500

    ss

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,7 @@
 
 echo 'Running collecstatic...'
 pwd
-git clone https://github.com/bagisto/bagisto.git 
-cd bagisto
-echo 'run bagisto'
-cat .env.example
-cp .env.example .env
-composer install
-php artisan bagisto:install --skip-env-check --skip-admin-creation
 
-php artisan migrate --force
-php artisan storage:link 
-php artisan db:seed --force || true 
-php artisan config:cache 
-php artisan route:cache 
-php artisan view:cache 
-php artisan optimize
-#php artisan serve --host=$HOST --port=80
+
+
+php artisan serve --host=$HOST --port=80
